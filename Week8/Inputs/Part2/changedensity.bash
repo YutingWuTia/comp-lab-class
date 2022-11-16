6a5e4bdd38820a8dafc2c4b430b174498468c2af
Yuting Wu <yw5806@log-2.hpc.nyu.edu>
2022-11-15 20:01:17 -0500

    add week8

---
+++ Week8/Inputs/Part2/changedensity.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=metadynamics
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --mem=20GB
+#SBATCH --time=10:00:00
+
+source /scratch/work/courses/CHEM-GA-2671-2022fa/software/lammps-gcc-30Oct2022/setup_lammps.bash
+
+cd /home/yw5806/comp-class/comp-lab-class/Week8/Inputs/Part2/
+
+densities_ranges=(0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5)
+ 
+
+for i in ${densities_ranges[@]};
+do 
+    mpirun -np 1 lmp -var density ${i} -in /home/yw5806/comp-class/comp-lab-class/Week8/Inputs/3dWCA.in -log density${i}_LOGFILE.log
+
+done
+
+
